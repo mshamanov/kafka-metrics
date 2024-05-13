258cd77430a326352f599fb6db29b155ae98f14b
Mikhail Shamanov <shamanovma@gmail.com>
2024-05-13 19:57:08 +0300

    Created initial project

---
+++ producer-service/src/main/java/com/mash/kafkametrics/producer/ProducerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mash.kafkametrics.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerServiceApplication.class, args);
+    }
+
+}
---
+++ producer-service/src/test/java/com/mash/kafkametrics/producer/ProducerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mash.kafkametrics.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
