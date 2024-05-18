a38a6af9a885d7fc64a8f5cb79e43b534dd66e93
Mikhail Shamanov <shamanovma@gmail.com>
2024-05-19 02:36:21 +0300

    Removed redundant imports

--- consumer-service/src/main/java/com/mash/kafkametrics/model/MetricsData.java
+++ consumer-service/src/main/java/com/mash/kafkametrics/model/MetricsData.java
@@ -11,8 +11,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
 
 /**
  * Class to represent metrics data.
