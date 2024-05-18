f36e536065f6b7482fc287243b1d824a90d0a5d6
Mikhail Shamanov <shamanovma@gmail.com>
2024-05-19 02:29:51 +0300

    Added version of the api into the path

--- consumer-service/src/main/java/com/mash/kafkametrics/controller/MetricsController.java
+++ consumer-service/src/main/java/com/mash/kafkametrics/controller/MetricsController.java
@@ -3,6 +3,7 @@ package com.mash.kafkametrics.controller;
 import com.mash.kafkametrics.model.MetricsData;
 import com.mash.kafkametrics.service.MetricsDataService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +17,7 @@ import java.util.List;
  */
 @Validated
 @RestController
-@RequestMapping("/api/metrics")
+@RequestMapping("/api/v1/metrics")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class MetricsController {
@@ -25,7 +26,7 @@ public class MetricsController {
     @GetMapping
     public ResponseEntity<?> getAllMetrics() {
         List<MetricsData> metricsData = this.service.findAll();
-        return ResponseEntity.ok(metricsData);
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(metricsData);
     }
 
     @GetMapping("/{id}")
@@ -36,6 +37,6 @@ public class MetricsController {
             return ResponseEntity.notFound().build();
         }
 
-        return ResponseEntity.ok(data);
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(data);
     }
 }
