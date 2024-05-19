e56dff49cafb0dac119c5ca3293db9c968c4df86
Mikhail Shamanov <shamanovma@gmail.com>
2024-05-19 17:59:58 +0300

    Added handler of ResponseStatusException

--- consumer-service/src/main/java/com/mash/kafkametrics/controller/advice/GlobalControllerAdvice.java
+++ consumer-service/src/main/java/com/mash/kafkametrics/controller/advice/GlobalControllerAdvice.java
@@ -5,6 +5,8 @@ import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
  * Global controller advice to handle various exceptions when interacting with the rest controller.
@@ -12,7 +14,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
  * @author Mikhail Shamanov
  */
 @ControllerAdvice
-public class GlobalControllerAdvice {
+public class GlobalControllerAdvice extends ResponseEntityExceptionHandler {
+    /**
+     * Handles {@link ResponseStatusException}.
+     *
+     * @param e instance of exception
+     * @return response
+     */
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<?> handleResponseException(ResponseStatusException e) {
+        return ResponseEntity.of(e.getBody()).build();
+    }
+
     /**
      * Handles any exception.
      *
@@ -20,7 +33,7 @@ public class GlobalControllerAdvice {
      * @return response
      */
     @ExceptionHandler
-    public ResponseEntity<?> handleException(Exception e) {
+    public ResponseEntity<?> handleAnyException(Exception e) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getLocalizedMessage());
         return ResponseEntity.of(problemDetail).build();
     }
